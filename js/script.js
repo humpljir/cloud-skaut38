aee977443ccd057845ec79ae050047cb7c620fe4
Jirka Humpl <jriihumpl@gmail.com>
2021-09-22 19:35:31 +0200

    menu functions and styling

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function closeMenu() {
+    document.getElementById("main-wrapper-div").classList.remove("menu-open");
+}
+
+function openMenu() {
+    document.getElementById("main-wrapper-div").classList.add("menu-open");
+}
