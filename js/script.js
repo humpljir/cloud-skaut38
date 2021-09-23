945053a343655180cd3e1c02835b6bda66fefa87
Jirka Humpl <jriihumpl@gmail.com>
2021-09-23 10:57:03 +0200

    different forms

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ function closeMenu() {
     document.getElementById("main-wrapper-div").classList.remove("menu-open");
 }
 
-function openMenu() {
+function openMenu(target) {
+    document.querySelectorAll("#menu-formbox-div form").forEach(element => {
+        element.classList.remove("form-visible");
+    });
+    document.getElementById(target).classList.add("form-visible");
     document.getElementById("main-wrapper-div").classList.add("menu-open");
 }
