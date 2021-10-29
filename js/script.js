083b47ffce043a2ddb8d47f3990e5cceebdeb9d1
humpljir <jirihumpl@gmail.com>
2021-10-29 15:33:09 +0200

    settings

--- js/script.js
+++ js/script.js
@@ -4,153 +4,184 @@ const hideTopBarOffset = 80;
 const alwaysTopBarOffset = 20;
 
 function closeMenu() {
-    document.getElementById("main-wrapper-div").classList.remove("menu-open");
-    document.getElementById("main-wrapper-div").classList.remove("icon-bar-hide");
+  document.getElementById("main-wrapper-div").classList.remove("menu-open");
 }
 
 function openMenu(target) {
-    document.querySelectorAll("#menu-formbox-div form").forEach(element => {
-        element.classList.remove("form-visible");
-    });
-    document.getElementById(target).classList.add("form-visible");
-    document.getElementById("menu-submit-button").innerHTML = document.getElementById(target).getAttribute("data-submit-label");
-    document.getElementById("menu-submit-button").setAttribute("form", target);
-    document.getElementById("main-wrapper-div").classList.add("menu-open");
-    document.getElementById("main-wrapper-div").classList.add("icon-bar-hide");
+  document.querySelectorAll("#menu-formbox-div form").forEach((element) => {
+    element.classList.remove("form-visible");
+  });
+  document.getElementById(target).classList.add("form-visible");
+  document.getElementById("menu-submit-button").innerHTML = document
+    .getElementById(target)
+    .getAttribute("data-submit-label");
+  document.getElementById("menu-submit-button").setAttribute("form", target);
+  document.getElementById("main-wrapper-div").classList.add("menu-open");
+}
+
+function openSide() {
+  document.documentElement.scrollTop = 0;
+  document.getElementById("main-wrapper-div").classList.add("side-open");
+  document.getElementById("main-wrapper-div").classList.add("top-bar-hide");
+  document.getElementById("main-wrapper-div").classList.add("icon-bar-hide");
+
+  setTimeout(() => {
+  document.getElementById("main-wrapper-div").classList.add("side-enable");
+  }, 1000);
+}
+
+function closeSide() {
+  document.documentElement.scrollTop = 0;
+  document.getElementById("main-wrapper-div").classList.remove("side-enable");
+  document.getElementById("main-wrapper-div").classList.remove("side-open");
+  document.getElementById("main-wrapper-div").classList.remove("top-bar-hide");
+  document.getElementById("main-wrapper-div").classList.remove("icon-bar-hide");
 }
 
 function openTopbar() {
-    document.getElementById("main-wrapper-div").classList.add("top-bar-open");
+  document.getElementById("main-wrapper-div").classList.add("top-bar-open");
 }
 
 function toggleDisplayStyle() {
-    document.getElementById("main-wrapper-div").classList.toggle("display-as-tiles");
+  document
+    .getElementById("main-wrapper-div")
+    .classList.toggle("display-as-tiles");
 }
 
 function drawDirectories() {
-    var fileSystem = "";
-    storage.forEach(element => {
-        var dir = '<button class="dir-box" style="--dir-color:' + element.color + '" data-dir-id="' + element.id + '" onclick=openDir(this)>' + element.name + '</button>';
-        fileSystem += dir;
-    });
-    document.getElementById("blank-canvas").innerHTML = fileSystem;
-    appendEmptyElements(20, document.getElementById("blank-canvas"),"dir-box");   
+  var fileSystem = "";
+  storage.forEach((element) => {
+    var dir =
+      '<button class="dir-box" style="--dir-color:' +
+      element.color +
+      '" data-dir-id="' +
+      element.id +
+      '" onclick=openDir(this)>' +
+      element.name +
+      "</button>";
+    fileSystem += dir;
+  });
+  document.getElementById("blank-canvas").innerHTML = fileSystem;
+  appendEmptyElements(20, document.getElementById("blank-canvas"), "dir-box");
 }
 
 function closeDir() {
-    blankCanvas = document.getElementById("blank-canvas");
+  blankCanvas = document.getElementById("blank-canvas");
 
-    blankCanvas.classList.add("blank-canvas");
+  blankCanvas.classList.add("blank-canvas");
 
-    setTimeout(() => {
-        drawDirectories();
-        blankCanvas.classList.remove("blank-canvas");
-    }, 400);
-
+  setTimeout(() => {
+    drawDirectories();
+    blankCanvas.classList.remove("blank-canvas");
+  }, 400);
 }
 
 function openDir(target) {
-    id = target.getAttribute("data-dir-id");
-    style = target.getAttribute("style");
-    var viewportOffset = target.getBoundingClientRect();
+  id = target.getAttribute("data-dir-id");
+  style = target.getAttribute("style");
+  var viewportOffset = target.getBoundingClientRect();
 
-    blankCanvas = document.getElementById("blank-canvas");
+  blankCanvas = document.getElementById("blank-canvas");
 
-    animatedDir = document.createElement("div");
-    animatedDir.innerHTML = target.innerHTML;
-    animatedDir.classList.add("dir-box-animated");
-    animatedDir.style = style;
-    animatedDir.style.left = viewportOffset.left + "px";
-    animatedDir.style.top = viewportOffset.top + "px";
-    animatedDir.style.width = target.offsetWidth + "px";
-    animatedDir.style.height = target.offsetHeight + "px";
-    animatedDirReturn = document.createElement("button");
-    animatedDirReturn.className = "dir-box-return";
-    animatedDirReturn.innerHTML = "< return";
-    animatedDirReturn.setAttribute("onClick", "closeDir()");
-    animatedDir.prepend(animatedDirReturn);
+  animatedDir = document.createElement("div");
+  animatedDir.innerHTML = target.innerHTML;
+  animatedDir.classList.add("dir-box-animated");
+  animatedDir.style = style;
+  animatedDir.style.left = viewportOffset.left + "px";
+  animatedDir.style.top = viewportOffset.top + "px";
+  animatedDir.style.width = target.offsetWidth + "px";
+  animatedDir.style.height = target.offsetHeight + "px";
+  animatedDirReturn = document.createElement("button");
+  animatedDirReturn.className = "dir-box-return";
+  animatedDirReturn.innerHTML = "< return";
+  animatedDirReturn.setAttribute("onClick", "closeDir()");
+  animatedDir.prepend(animatedDirReturn);
 
-    document.getElementById("window-scroll-div").append(animatedDir);
+  document.getElementById("window-scroll-div").append(animatedDir);
 
-    blankCanvas.classList.add("blank-canvas");
+  blankCanvas.classList.add("blank-canvas");
+
+  setTimeout(() => {
+    animatedDir.classList.add("dir-box-expanded");
+    animatedDir.style.left = "";
+    animatedDir.style.top = "";
+    animatedDir.style.width = "";
+    animatedDir.style.height = "";
 
     setTimeout(() => {
-        animatedDir.classList.add("dir-box-expanded");
-        animatedDir.style.left = "";
-        animatedDir.style.top = "";
-        animatedDir.style.width = "";
-        animatedDir.style.height = "";
+      document.documentElement.scrollTop = 0;
 
-        setTimeout(() => {
-            document.documentElement.scrollTop = 0;
+      drawFiles(id);
 
-            drawFiles(id);
+      headerDir = animatedDir.cloneNode(true);
+      headerDir.className = "dir-header";
+      headerSpacer = document.createElement("div");
+      headerSpacer.className = "dir-header-spacer";
+      blankCanvas.prepend(headerSpacer);
+      blankCanvas.prepend(headerDir);
 
-            headerDir = animatedDir.cloneNode(true);
-            headerDir.className = "dir-header";
-            headerSpacer = document.createElement("div");
-            headerSpacer.className = "dir-header-spacer";
-            blankCanvas.prepend(headerSpacer);
-            blankCanvas.prepend(headerDir);
+      blankCanvas.classList.remove("blank-canvas");
 
-            blankCanvas.classList.remove("blank-canvas");
-
-            setTimeout(() => {
-                animatedDir.remove();
-            }, 400);
-        }, 400);
-    }, 100);
+      setTimeout(() => {
+        animatedDir.remove();
+      }, 400);
+    }, 400);
+  }, 100);
 }
 
 function drawFiles(dirID) {
-    console.log(dirID);
-    var filesList = storage.find(function (post, index) {
-        if (post.id == dirID)
-            return true;
-    });
-    console.log(FileList);
+  console.log(dirID);
+  var filesList = storage.find(function (post, index) {
+    if (post.id == dirID) return true;
+  });
+  console.log(FileList);
 
-    var fileSystem = "";
-    filesList.content.forEach(element => {
-        var dir = '<div class="file-box-flex"><button class="file-box"><div class="file-icon"></div><div class="file-label">' + element.name + '</div></button></div>';
-        fileSystem += dir;
-    });
+  var fileSystem = "";
+  filesList.content.forEach((element) => {
+    var dir =
+      '<div class="file-box-flex"><button class="file-box"><div class="file-icon"></div><div class="file-label">' +
+      element.name +
+      "</div></button></div>";
+    fileSystem += dir;
+  });
 
-    document.getElementById("blank-canvas").innerHTML = fileSystem;
-    appendEmptyElements(20, document.getElementById("blank-canvas"), "file-box");
+  document.getElementById("blank-canvas").innerHTML = fileSystem;
+  appendEmptyElements(20, document.getElementById("blank-canvas"), "file-box");
 }
 
-function appendEmptyElements(n,target,chameleonClass) {
-    for (let index = 0; index < n; index++) {
-emptyEl = document.createElement("div");
-emptyEl.className = chameleonClass + " empty-flex-element";
-target.append(emptyEl);
-    }
+function appendEmptyElements(n, target, chameleonClass) {
+  for (let index = 0; index < n; index++) {
+    emptyEl = document.createElement("div");
+    emptyEl.className = chameleonClass + " empty-flex-element";
+    target.append(emptyEl);
+  }
 }
 
 function initialize() {
-    drawDirectories();
+  drawDirectories();
 
-    var scrollYLast = 0;
-    var scrollYDistance = 0;
-    //var topBarOffsetSum = 0;
+  var scrollYLast = 0;
+  var scrollYDistance = 0;
+  //var topBarOffsetSum = 0;
 
-    document.addEventListener("scroll", (e) => {
-        var scrollY = document.documentElement.scrollTop;
+  document.addEventListener("scroll", (e) => {
+    var scrollY = document.documentElement.scrollTop;
 
-        if (Math.sign(scrollYDistance) == Math.sign(scrollYLast - scrollY)) {
-            scrollYDistance += scrollYLast - scrollY;
-        } else {
-            scrollYDistance = scrollYLast - scrollY;
-        }
+    if (Math.sign(scrollYDistance) == Math.sign(scrollYLast - scrollY)) {
+      scrollYDistance += scrollYLast - scrollY;
+    } else {
+      scrollYDistance = scrollYLast - scrollY;
+    }
 
-        if (scrollYDistance > hideTopBarOffset || scrollY < alwaysTopBarOffset) {
-            document.getElementById("main-wrapper-div").classList.add("top-bar-open");
-        } else if (scrollYDistance < (-1) * hideTopBarOffset) {
-            document.getElementById("main-wrapper-div").classList.remove("top-bar-open");
-        }
+    if (scrollYDistance > hideTopBarOffset || scrollY < alwaysTopBarOffset) {
+      document.getElementById("main-wrapper-div").classList.add("top-bar-open");
+    } else if (scrollYDistance < -1 * hideTopBarOffset) {
+      document
+        .getElementById("main-wrapper-div")
+        .classList.remove("top-bar-open");
+    }
 
-        /*
+    /*
         if (topBarOffsetSum > 0) {
             document.getElementById("main-wrapper-div").classList.add("top-bar-open");
         } else if (scrollY >= hideTopBarOffset) {
@@ -166,8 +197,8 @@ function initialize() {
         console.log(scrollYDistance);
         */
 
-        scrollYLast = scrollY;
-    });
+    scrollYLast = scrollY;
+  });
 }
 
 initialize();
