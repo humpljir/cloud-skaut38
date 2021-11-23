84a4ab1f6d9e4a6deb3390a0fee3f8425fcf7cbe
humpljir <jirihumpl@gmail.com>
2021-11-23 19:51:15 +0100

    customNotifications wrapper

---
+++ js/customNotifications.js
@@ -0,0 +1,5 @@
+function initCustomNotifications() {
+  let customNotificationsWrapper = document.createElement("div");
+  customNotificationsWrapper.className="custom-notifications-wrapper";
+  document.getElementById("window-div").append(customNotificationsWrapper);
+}
