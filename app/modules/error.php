09ae2c3038cbdf15e1a4a93045d42ab52fd1dbbd
humpljir <jirihumpl@gmail.com>
2021-12-31 01:47:55 +0100

    trying to fix bug with uplaoding files

--- app/modules/error.php
+++ app/modules/error.php
@@ -1,9 +1,9 @@
 <?php
 $global_error = '';
 
-function add_global_error($error_msg)
+function add_global_error($error_msg,$color)
 {
     global $global_error;
-    $global_error .= 'pushCustomNotifications("' . $error_msg . '","var(--notifications-confirm-color)");';
+    $global_error .= 'pushCustomNotifications("' . $error_msg . '","'.$color.'");';
 }
 ?>
--- app/modules/files.php
+++ app/modules/files.php
@@ -4,15 +4,15 @@ function file_delete($id) {
     $del_path = mysqli_query($mysqli, "SELECT url FROM files WHERE id='$id'");
     while ($path = mysqli_fetch_array($del_path)) {
         if (unlink("../files/uploads/" . $path['link'])) {
-            add_global_error("Soubor byl odstraněn!");
+            add_global_error("Soubor byl odstraněn!","var(--notifications-regular-color)");
             $sql = "DELETE FROM soubory WHERE id='$id'";
             if ($mysqli->query($sql) === TRUE) {
-                add_global_error("Záznam v databázi byl odstraněn!");
+                add_global_error("Záznam v databázi byl odstraněn!","var(--notifications-regular-color)");
             } else {
-                add_global_error("Error v databázi: " . $mysqli->error);
+                add_global_error("Error v databázi: " . $mysqli->error,"var(--notifications-error-color)");
             }
         } else {
-            add_global_error("Error mazání souboru. ID='" . $id . " url= " . $path['url']);
+            add_global_error("Error mazání souboru. ID='" . $id . " url= " . $path['url'],"var(--notifications-error-color)");
         }
     }
 }
@@ -21,18 +21,17 @@ function file_upload($name) {
     echo "<script>console.log('uploading file')</script>";
     $target_dir = "data/storage/";
     $target_file = $target_dir.basename($_FILES["file_upload"]["name"]);
-    echo $target_file;
     $uploadError = 0;
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
     // Check if file already exists
     if (file_exists($target_file)) {
-        add_global_error("Tento soubor už jednou nahrán byl.");
+        add_global_error("Tento soubor už jednou nahrán byl.","var(--notifications-warning-color)");
         $uploadError = 1;
     }
     // Check if $uploadOk is set to 0 by an error
     if ($uploadError == 1) {
-        add_global_error("Soubor nebyl nahrán!");
+        add_global_error("Soubor nebyl nahrán!","var(--notifications-error-color)");
         // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["file_upload"]["tmp_name"], $target_file)) {
@@ -40,10 +39,10 @@ function file_upload($name) {
         VALUES ('$name', '1640867409', 'unknown', '$target_file', 'unknown', '1000', '0')";
 
             if ($mysqli->query($sql) !== TRUE) {
-                add_global_error("Error uplaoding file: " . $mysqli->error);
+                add_global_error("Error uplaoding file: " . $mysqli->error,"var(--notifications-warning-color)");
             }
         } else {
-            add_global_error("Sorry, there was an error uploading your file.");
+            add_global_error("Sorry, there was an error uploading your file.","var(--notifications-error-color)");
         }
     }
 }
